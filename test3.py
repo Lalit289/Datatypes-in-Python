4c00c47584acb849989fcddd0cab84b97d5904d6
Lalit289 <87890683+Lalit289@users.noreply.github.com>
2021-12-28 20:38:02 +0530

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,18 @@
+# Datatype
+
+
+
+# Integer
+#string
+#Float
+#Boolean
+
+db = 78
+db2 = "Lalit Is good boy"
+db3 = 56.463
+db4 = True 
+
+print(type(db))
+print(type(db2))
+print(type(db3))
+print(type(db4))
